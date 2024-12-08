602192776b3d86426206de8041c2d8c9cb2b1a48
Marcelo Oroquieta <oroquieta.m@gmail.com>
2024-12-08 20:59:56 -0300

    adding final files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastProvider } from './context/ToastContext';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Reservations from './pages/Reservations';
+import OrderOnline from './pages/OrderOnline';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <Navbar className="container mx-auto" />
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-    </div>
+    <Router>
+      <ToastProvider>
+        <div className="flex min-h-screen flex-col">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/reservations" element={<Reservations />} />
+              <Route path="/order-online" element={<OrderOnline />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </ToastProvider>
+    </Router>
   );
 }
+
 export default App;
