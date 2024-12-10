3ff997a64f69ff122b88d8138a13c04605a99b5e
Marcelo Oroquieta <oroquieta.m@gmail.com>
2024-12-10 13:59:32 -0300

    chore: change vite config and update uploaded version

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/final-capstone/',
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
+  },
+  build: {
+    outDir: 'docs',
   },
 });
